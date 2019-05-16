7e1b13561f4d50d0d91d03606d70d3c00db2db71
Stefano Cascavilla <stefanocascavilla@iMac-di-Stefano.station>
2019-05-16 18:21:44 +0200

    Created method which creates the EM by using only correct events (filtered by login status and date)

--- EmotionalMaps.java
+++ EmotionalMaps.java
@@ -23,11 +23,46 @@ public class EmotionalMaps {
         }
     }
 
+    // This method creates the filtered emotional map (it checks that event is related to a logged user and it checks that the event has been made between the two argument dates)
+    public static void generateFilteredEmotionalMaps (ArrayList<Event> eventsList, PointOfInterest[] pointsOfInterest, Date firstLimit, Date secondLimit) {
+        double[] distances = new double[pointsOfInterest.length]; // Defining distances array for getting distances between POIs and coordinates related to an event
+        int pointOfInterestIndex = 0; // This variable is used to understand which is the index of the smallest item into the distances array
+
+        ArrayList<Event> filteredEventsList = filterEvents(eventsList, firstLimit, secondLimit); // Getting the array which contains only the correct events
+
+        for (Event singleEvent : filteredEventsList) {
+            // For each event, the function calculates the distances between each POI and the event
+            for (int count = 0; count < pointsOfInterest.length; count++) {
+                distances[count] = pointsOfInterest[count].getCoordinatePOI().getDistanceBetweenTwoPoints(singleEvent.getEventCoordinate());
+            }
+            pointOfInterestIndex = findSmallestIndex(distances); // Getting the index of the smallest item into the distances array
+            pointsOfInterest[pointOfInterestIndex].getRelatedEmotions().increase(singleEvent.getEmotion()); // With the found index, the function is going to get the nearest POI at the event's coordinate. Then it increases the emotion related to that POI
+        }
+
+        // For each POI the function prints the line with the percentages for each kind of emotion
+        for (PointOfInterest singlePOI : pointsOfInterest) {
+            printSinglePOILine(singlePOI, eventsList.size());
+        }
+    }
+
 
     /*
      Helper functions: functions which I've created for divide the code better
      These functions are available only from methods within this class
     */
+
+    // This function filters the events and returns only those which have been made between two dates and by logged users
+    private static ArrayList<Event> filterEvents (ArrayList<Event> eventsList, Date firstLimit, Date secondLimit) {
+        ArrayList<Event> returnArray = new ArrayList<Event>();
+
+        for (Event item : eventsList) {
+            if (item.getLogin() && ((item.getDate().compareTo(firstLimit) == 1 || item.getDate().compareTo(firstLimit) == 0) && (item.getDate().compareTo(secondLimit) == -1 || item.getDate().compareTo(secondLimit) == 0))) {
+                returnArray.add(item);
+            }
+        }
+
+        return returnArray;
+    }
 
     // This functions returns the index of the smallest value into the array
     private static int findSmallestIndex (double[] distances) {
